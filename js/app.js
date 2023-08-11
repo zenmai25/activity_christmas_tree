052f4c5f43f2f3b8d84504d25cc2f2346a71db0c
zenmai25 <arvindespi@gmail.com>
2023-08-11 15:10:53 +0800

    christmas_tree
    activity

---
+++ js/app.js
@@ -0,0 +1,153 @@
+//example code
+
+// var n = 8;
+ 
+// for(i=1; i<=n; i++)
+// { 
+//     //Prints trailing spaces
+//     for(j=i; j<n; j++)
+//     {
+//         document.write(" ");
+//     }
+
+//     //Prints the pyramid pattern
+//     for(j=1; j<=(2*i-1); j++)
+//     {
+//         document.write("*");
+//     }
+
+//     document.write("<br>");
+
+//     }
+
+console.log('Hello World!')
+// first
+for(i=1; i<=0; i++){
+    for(j=i; j<n; j++){
+        document.write(" ");
+    }
+}
+
+for(j=1; j<=i; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+    
+//second
+
+
+for(j=1; j<=3; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+  
+
+//third
+
+for(j=1; j<=5; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//fourth
+
+for(j=1; j<=7; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//fifth
+
+for(j=1; j<=9; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//sixth
+
+for(j=1; j<=11; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//seventh
+
+for(j=1; j<=13; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//eighth
+
+for(j=1; j<=15; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//ninth
+
+for(j=1; j<=17; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//tenth
+
+for(j=1; j<=19; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//11th
+
+for(j=1; j<=21; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//12th
+
+for(j=1; j<=23; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+//13th
+
+for(j=1; j<=24; j++){
+    document.write("*");
+}
+{
+    document.write("<br>")
+}
+
+// 14th
+
+for(j=1; j<=26; j++){
+    document.write("*");
+}
+
